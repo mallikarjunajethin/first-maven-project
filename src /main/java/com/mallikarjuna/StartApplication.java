e7f3b41553ed813a5b2ffa3c68352844859c6737
mallikarjunajethin <159537551+mallikarjunajethin@users.noreply.github.com>
2024-05-12 15:51:26 +0530

    Update StartApplication.java

--- src /main/java/com/mallikarjuna/StartApplication.java
+++ src /main/java/com/mallikarjuna/StartApplication.java
@@ -1,22 +1,16 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+package com.mycompany.app;
 
-@SpringBootApplication
-@Controller
-public class StartApplication {
+public class App {
 
-    @GetMapping("/")
-    public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
-        return "index";
-    }
+    private static final String MESSAGE = "Hello World!";
+
+    public App() {}
 
     public static void main(String[] args) {
-        SpringApplication.run(StartApplication.class, args);
+        System.out.println(MESSAGE);
     }
 
+    public String getMessage() {
+        return MESSAGE;
+    }
 }
